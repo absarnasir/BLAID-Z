9709d81e6da2bbd7b74b46a964055cbc50170fde
absarnasir <76106313+absarnasir@users.noreply.github.com>
2021-09-19 09:23:42 -0700

    HI
    thanks

---
+++ js/style.js
@@ -0,0 +1,147 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navbar_top').classList.add('fixedtop');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+          document.getElementById('navbar_top').classList.remove('fixedtop');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+    });
+  }); 
+  
+
+  //menu a color white to black//
+
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor').classList.add('black');
+          
+
+          document.getElementById('navcolor').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor').classList.add('white');
+         
+          document.getElementById('navcolor').classList.remove('black');
+          
+        } 
+    });
+  }); 
+
+//menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor1').classList.add('black');
+          
+
+          document.getElementById('navcolor1').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor1').classList.add('white');
+         
+          document.getElementById('navcolor1').classList.remove('black');
+          
+        } 
+    });
+  }); 
+//menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor2').classList.add('black');
+          
+
+          document.getElementById('navcolor2').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor2').classList.add('white');
+         
+          document.getElementById('navcolor2').classList.remove('black');
+          
+        } 
+    });
+  }); 
+//menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor3').classList.add('black');
+          
+
+          document.getElementById('navcolor3').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor3').classList.add('white');
+         
+          document.getElementById('navcolor3').classList.remove('black');
+          
+        } 
+    });
+  }); 
+
+//menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor4').classList.add('black');
+          
+
+          document.getElementById('navcolor4').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor4').classList.add('white');
+         
+          document.getElementById('navcolor4').classList.remove('black');
+          
+        } 
+    });
+  }); 
+//menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor5').classList.add('black');
+          
+
+          document.getElementById('navcolor5').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor5').classList.add('white');
+         
+          document.getElementById('navcolor5').classList.remove('black');
+          
+        } 
+    });
+  }); 
+
+  //menu a color white to black//
+  document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 500) {
+          document.getElementById('navcolor6').classList.add('black');
+          
+
+          document.getElementById('navcolor6').classList.remove('white');
+           
+        
+        } else {
+          document.getElementById('navcolor6').classList.add('white');
+         
+          document.getElementById('navcolor6').classList.remove('black');
+          
+        } 
+    });
+  }); 
